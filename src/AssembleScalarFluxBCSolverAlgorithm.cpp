6d8250a3d6b82a377eb72e14737e2260363d05af
Raphael Lindegger <raphiluzern@gmx.ch>
2019-05-07 10:48:15 +0200

    documented Neumann BC alg. and tested

--- src/AssembleScalarFluxBCSolverAlgorithm.cpp
+++ src/AssembleScalarFluxBCSolverAlgorithm.cpp
@@ -52,128 +52,125 @@ AssembleScalarFluxBCSolverAlgorithm::initialize_connectivity()
 //--------------------------------------------------------------------------
 //-------- execute ---------------------------------------------------------
 //--------------------------------------------------------------------------
-void
-AssembleScalarFluxBCSolverAlgorithm::execute()
-{
+void AssembleScalarFluxBCSolverAlgorithm::execute() {
+    stk::mesh::BulkData & bulk_data = realm_.bulk_data();
+    stk::mesh::MetaData & meta_data = realm_.meta_data();
 
-  stk::mesh::BulkData & bulk_data = realm_.bulk_data();
-  stk::mesh::MetaData & meta_data = realm_.meta_data();
-
-  const int nDim = meta_data.spatial_dimension();
+    const int nDim = meta_data.spatial_dimension();
 
 
-  // space for LHS/RHS; nodesPerFace*nodesPerFace and nodesPerFace
-  std::vector<double> lhs;
-  std::vector<double> rhs;
-  std::vector<int> scratchIds;
-  std::vector<double> scratchVals;
-  std::vector<stk::mesh::Entity> connected_nodes;
+    // space for LHS/RHS; nodesPerFace*nodesPerFace and nodesPerFace
+    std::vector<double> lhs;
+    std::vector<double> rhs;
+    std::vector<int> scratchIds;
+    std::vector<double> scratchVals;
+    std::vector<stk::mesh::Entity> connected_nodes;
 
-  // nodal fields to gather
-  std::vector<double> ws_bcScalarQ;
+    // nodal fields to gather
+    std::vector<double> ws_bcScalarQ;
 
-  // master element
-  std::vector<double> ws_face_shape_function;
+    // master element
+    std::vector<double> ws_face_shape_function;
 
-  // define some common selectors
-  stk::mesh::Selector s_locally_owned_union = meta_data.locally_owned_part()
-    &stk::mesh::selectUnion(partVec_);
+    // define some common selectors
+    stk::mesh::Selector s_locally_owned_union = meta_data.locally_owned_part()
+      &stk::mesh::selectUnion(partVec_);
 
-  stk::mesh::BucketVector const& face_buckets =
-    realm_.get_buckets( meta_data.side_rank(), s_locally_owned_union );
-  for ( stk::mesh::BucketVector::const_iterator ib = face_buckets.begin();
-        ib != face_buckets.end() ; ++ib ) {
-    stk::mesh::Bucket & b = **ib ;
+    // Iterate through selected buckets
+    stk::mesh::BucketVector const & face_buckets = realm_.get_buckets( meta_data.side_rank(), s_locally_owned_union );
+    for ( stk::mesh::BucketVector::const_iterator ib = face_buckets.begin(); ib != face_buckets.end() ; ++ib ) {
+        stk::mesh::Bucket & b = **ib ;
 
-    // face master element
-    MasterElement *meFC = MasterElementRepo::get_surface_master_element(b.topology());
-    const int nodesPerFace = meFC->nodesPerElement_;
-    const int numScsBip = meFC->numIntPoints_;
+        // face master element
+        MasterElement *meFC = MasterElementRepo::get_surface_master_element(b.topology());
+        const int nodesPerFace = meFC->nodesPerElement_;
+        const int numScsBip = meFC->numIntPoints_;
 
-    // mapping from ip to nodes for this ordinal; face perspective (use with face_node_relations)
-    const int *faceIpNodeMap = meFC->ipNodeMap();
+        // mapping from ip to nodes for this ordinal; face perspective (use with face_node_relations)
+        const int *faceIpNodeMap = meFC->ipNodeMap();
 
-    // resize some things; matrix related
-    const int lhsSize = nodesPerFace*nodesPerFace;
-    const int rhsSize = nodesPerFace;
-    lhs.resize(lhsSize);
-    rhs.resize(rhsSize);
-    scratchIds.resize(rhsSize);
-    scratchVals.resize(rhsSize);
-    connected_nodes.resize(nodesPerFace);
+        // resize some things; matrix related
+        const int lhsSize = nodesPerFace*nodesPerFace;
+        const int rhsSize = nodesPerFace;
+        lhs.resize(lhsSize);
+        rhs.resize(rhsSize);
+        scratchIds.resize(rhsSize);
+        scratchVals.resize(rhsSize);
+        connected_nodes.resize(nodesPerFace);
 
-    // algorithm related; element
-    ws_bcScalarQ.resize(nodesPerFace);
-    ws_face_shape_function.resize(numScsBip*nodesPerFace);
+        // algorithm related; element
+        ws_bcScalarQ.resize(nodesPerFace);
+        ws_face_shape_function.resize(numScsBip*nodesPerFace);
 
-    // pointers
-    double *p_lhs = &lhs[0];
-    double *p_rhs = &rhs[0];
-    double *p_bcScalarQ = &ws_bcScalarQ[0];
-    double *p_face_shape_function = &ws_face_shape_function[0];
+        // pointers
+        double *p_lhs = &lhs[0];
+        double *p_rhs = &rhs[0];
+        double *p_bcScalarQ = &ws_bcScalarQ[0];
+        double *p_face_shape_function = &ws_face_shape_function[0];
 
-    // shape functions
-    meFC->shape_fcn(&p_face_shape_function[0]);
+        // shape functions
+        meFC->shape_fcn(&p_face_shape_function[0]);
 
-    const size_t length   = b.size();
+        const size_t length   = b.size();
 
-    for ( size_t k = 0 ; k < length ; ++k ) {
+        // Iterate though each boundary face in bucket
+        for ( size_t k = 0 ; k < length ; ++k ) {
+            // zero lhs/rhs
+            for ( int p = 0; p < lhsSize; ++p )
+                p_lhs[p] = 0.0;
+            for ( int p = 0; p < rhsSize; ++p )
+                p_rhs[p] = 0.0;
 
-      // zero lhs/rhs
-      for ( int p = 0; p < lhsSize; ++p )
-        p_lhs[p] = 0.0;
-      for ( int p = 0; p < rhsSize; ++p )
-        p_rhs[p] = 0.0;
+            // get face
+            stk::mesh::Entity face = b[k];
 
-      // get face
-      stk::mesh::Entity face = b[k];
+            //======================================
+            // gather nodal data off of face
+            //======================================
+            stk::mesh::Entity const * face_node_rels = bulk_data .begin_nodes(face);
+            int num_face_nodes = bulk_data.num_nodes(face);
+            // sanity check on num nodes
+            ThrowAssert( num_face_nodes == nodesPerFace );
 
-      //======================================
-      // gather nodal data off of face
-      //======================================
-      stk::mesh::Entity const * face_node_rels = bulk_data .begin_nodes(face);
-      int num_face_nodes = bulk_data.num_nodes(face);
-      // sanity check on num nodes
-      ThrowAssert( num_face_nodes == nodesPerFace );
-      for ( int ni = 0; ni < num_face_nodes; ++ni ) {
+            // Make list of connected nodes and get scalar values of the nodes
+            for ( int ni = 0; ni < num_face_nodes; ++ni ) {
+                // get the node and form connected_node
+                stk::mesh::Entity node = face_node_rels[ni];
+                connected_nodes[ni] = node;
 
-        // get the node and form connected_node
-        stk::mesh::Entity node = face_node_rels[ni];
-        connected_nodes[ni] = node;
+                // gather scalar
+                p_bcScalarQ[ni] = *stk::mesh::field_data(*bcScalarQ_, node);
+            }
 
-        // gather scalar
-        p_bcScalarQ[ni] = *stk::mesh::field_data(*bcScalarQ_, node);
-      }
+            // pointer to face data
+            double * areaVec = stk::mesh::field_data(*exposedAreaVec_, face);
 
-      // pointer to face data
-      double * areaVec = stk::mesh::field_data(*exposedAreaVec_, face);
+            // Iterate through each integration point
+            for ( int ip = 0; ip < numScsBip; ++ip ) {
+                const int localFaceNode = faceIpNodeMap[ip];
+                const int offSetSF_face = ip*nodesPerFace;
 
-      // loop over face nodes
-      for ( int ip = 0; ip < numScsBip; ++ip ) {
+                // interpolate to bip
+                double fluxBip = 0.0;
 
-        const int localFaceNode = faceIpNodeMap[ip];
+                // Iterate through each node of the face
+                for ( int ic = 0; ic < nodesPerFace; ++ic ) {
+                    const double r = p_face_shape_function[offSetSF_face+ic];
+                    fluxBip += r*p_bcScalarQ[ic];
+                }
 
-        const int offSetSF_face = ip*nodesPerFace;
+                const int offset = ip*nDim;
+                double areaNorm = 0.0;
+                for (int idir = 0; idir < nDim; ++idir) {
+                    areaNorm += areaVec[offset+idir]*areaVec[offset+idir];
+                }
+                areaNorm = std::sqrt(areaNorm);
 
-        // interpolate to bip
-        double fluxBip = 0.0;
-        for ( int ic = 0; ic < nodesPerFace; ++ic ) {
-          const double r = p_face_shape_function[offSetSF_face+ic];
-          fluxBip += r*p_bcScalarQ[ic];
+                p_rhs[localFaceNode] += fluxBip*areaNorm;
+            }
+
+            apply_coeff(connected_nodes, scratchIds, scratchVals, rhs, lhs, __FILE__);
         }
-
-        const int offset = ip*nDim;
-        double areaNorm = 0.0;
-        for (int idir = 0; idir < nDim; ++idir)
-          areaNorm += areaVec[offset+idir]*areaVec[offset+idir];
-        areaNorm = std::sqrt(areaNorm);
-
-        p_rhs[localFaceNode] += fluxBip*areaNorm;
-
-      }
-
-      apply_coeff(connected_nodes, scratchIds, scratchVals, rhs, lhs, __FILE__);
     }
-  }
 }
 
